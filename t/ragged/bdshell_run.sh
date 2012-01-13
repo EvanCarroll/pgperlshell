3cda51850b07a96631a2e5d1afa0db625796793c
Evan Carroll <me@evancarroll.com>
2012-01-12 19:35:11 -0600

    added new featuer PERL_static_column permitting you to supply values for some columns, added tests and docs, add MANIFEST to gitignore, fixed two pod errors

--- t/ragged/bdshell_run.sh
+++ t/ragged/bdshell_run.sh
@@ -1,8 +1,16 @@
 bdshell -d dealermade -c 'TRUNCATE TABLE bdshell_test.ragged;'
 
 ## Should die no idea how many columns the table is, can't trucate data
+echo "This should error:";
 bdshell -d dealermade -c '\COPY bdshell_test.ragged FROM data/ten_column_load_normal.csv WITH -PERL_ragged'
 
+## This file has two values, "1,3" static inserts responsible for 2 and 4
+bdshell -d dealermade -c '\COPY bdshell_test.ragged (a,b,c,d) FROM data/two_column_load_normal.csv WITH -PERL_static_column=$${"b": 2, "d": 4}$$;'
+
+bdshell -d dealermade -c '\COPY bdshell_test.ragged (a,b,c,d) FROM data/ten_column_load_normal.csv WITH -PERL_ragged -PERL_static_column=$${"d": 4}$$;'
+
+echo "This should error:";
+bdshell -d dealermade -c '\COPY bdshell_test.ragged (a,b,c,d) FROM data/ten_column_load_normal.csv WITH -PERL_static_column=$${"d": 4}$$;'
 
 ## All of these input files should insert 1,2,3,4
 bdshell -d dealermade -c '\COPY bdshell_test.ragged ( a, b, c, d ) FROM data/ten_column_load_normal.csv WITH -PERL_ragged'
