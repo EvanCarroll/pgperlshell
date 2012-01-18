f2752b8fd4b65e7a8aa59b663b3cef1cc3fd06f9
Evan Carroll <me@evancarroll.com>
2012-01-18 13:35:42 -0600

    broke apart the shell into a backend, and a DB Role; prepairing to pull out CSV stuff

---
+++ lib/bdshell/Roles/DB.pm
@@ -0,0 +1,58 @@
+package bdshell::Roles::DB;
+use strict;
+use warnings;
+use Moose::Role;
+
+use feature ':5.10';
+use v5.10;
+
+use namespace::autoclean;
+
+has '_dbh' => (
+	isa  => 'Object'
+	, is => 'ro'
+	, lazy => 1
+	, default => sub {
+		my $self = shift;
+		my $db = $self->_db;
+		my $dbh = DBI->connect_cached("dbi:Pg:dbname=$db"
+			, $self->_un
+			, $self->_pw, {RaiseError=>1}
+		) or die $DBI::errstr;
+
+		if ( $dbh->ping == 1 ) {
+			say 'Connection established' if $bdshell::DEBUG;
+		}
+		else {
+			die 'not connected';
+		}
+
+		$dbh;
+
+	}
+);
+
+has '_un' => (
+	isa => 'Str'
+	, is => 'ro'
+	, default => $ENV{USER}
+);
+
+has '_pw' => (
+	isa => 'Maybe[Str]'
+	, is => 'ro'
+);
+
+has '_db' => (
+	isa => 'Str'
+	, is => 'ro'
+	, default => $ENV{USER}
+);
+
+sub db_set {
+	my ( $self, $k, $v ) = @_;
+	my $dbh = $self->_dbh;
+	$dbh->do( sprintf( 'SET %s = %s;', $k, $v ) );
+}
+
+1;
